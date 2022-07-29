9e6d77fae4a495810c952ba4e96ff1f60a11f473
Jo-yh96 <cho845k@gmail.com>
2022-07-29 16:21:29 +0900

    0728 dir 삭제

--- 0728/server/utils/async-handler.js
+++
@@ -1,9 +0,0 @@
-module.exports = (requsetHandler) => {
-    return async (req, res, next) => {
-        try {
-            await requsetHandler(req,res);
-        }catch(err) {
-            next(err);
-        }
-    }
-}
